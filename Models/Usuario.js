2b20807d29a21c0d6d5b2538e6e70f792b9e7f60
CodingCupcake <brjezunigalo@ittepic.edu.mx>
2024-02-21 21:12:27 -0700

    Modelo usuario agregado

---
+++ Models/Usuario.js
@@ -0,0 +1,8 @@
+
+module.exports = class Usuario {
+    constructor(id, nombre, contraseña) {
+            this.id = id;
+            this.nombre = nombre;
+            this.contraseña = contraseña;
+        }
+};
